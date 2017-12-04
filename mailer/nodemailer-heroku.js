af0e08a608de76373812118d8f440cce665c12b7
lbianqui <lucas.bianqui1992@gmail.com>
2017-12-04 18:02:29 -0300

    config db y mailer.

--- mailer/nodemailer-heroku.js
+++ mailer/nodemailer-heroku.js
@@ -1,12 +1,15 @@
 const db = require('../db');
 var nodemailer = require('nodemailer');
 var mg = require('nodemailer-mailgun-transport');
+var config = require('config');
 
 // This is your API key that you retrieve from www.mailgun.com/cp (free up to 10K monthly emails)
+var dbConfigMailer = config.get('mailer.dbConfigMailer'); // from default.json
+
 var auth = {
   auth: {
-    api_key: 'key-efa6e6989ca64a65ee2c4aecf4ef535d',
-    domain: 'sandbox31540e7949194389b6ba35fcd8fb443a.mailgun.org'
+    api_key: dbConfigMailer.key,
+    domain: dbConfigMailer.domain
   }
 }
 
